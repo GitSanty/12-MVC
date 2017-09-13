faf66492b6bbb8acbc44d924e8103a686c67ebf6
Santiago Astete <sastetec@hotmail.com>
2017-09-13 16:40:52 +0200

    Add files via upload

---
+++ views/introduction_dev_view.php
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <header>
+      <a href='http://localhost/12-MVC/'>Home</a>
+    </header>
+
+     
+    <footer>
+      Copyright
+    </footer>
+  <body>
+<html>
---
+++ views/making_of_view.php
@@ -0,0 +1,16 @@
+<DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <header>
+      <a href='http://localhost/12-MVC/'>Home</a>
+    </header>
+
+    <?php require_once('router.php'); ?>
+
+    <footer>
+      Copyright
+    </footer>
+  <body>
+<html>
